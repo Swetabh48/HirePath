a61b5dee36babccd77a6aa6d42ed5a083a5779e9
Swetabh Salampuria <swetabhsalampuria48@gmail.com>
2025-06-17 02:28:42 +0530

    setup Auth pages

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Mona_Sans} from "next/font/google";
 import "./globals.css";
-
+import { Toaster } from "@/components/ui/sonner";
 const monaSans = Mona_Sans({
   variable: "--font-mona-sans",
   subsets: ["latin"],
@@ -24,6 +24,8 @@ export default function RootLayout({
         className={`${monaSans.className} antialiased pattern`}
       >
         {children}
+
+        <Toaster/>
       </body>
     </html>
   );
