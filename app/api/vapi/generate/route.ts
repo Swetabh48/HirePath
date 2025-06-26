c138c554ba247f9fac84d20a29b5f53e3dea5317
Swetabh Salampuria <swetabhsalampuria48@gmail.com>
2025-06-26 08:58:37 +0530

    refactored vapi generate again

--- app/api/vapi/generate/route.ts
+++ app/api/vapi/generate/route.ts
@@ -9,8 +9,8 @@ export async function POST(request: Request) {
 
   try {
     const { text: questions } = await generateText({
-      model: google("gemini-2.5-flash"),
-      prompt: Prepare questions for a job interview.
+      model: google("gemini-2.0-flash-001"),
+      prompt: `Prepare questions for a job interview.
         The job role is ${role}.
         The job experience level is ${level}.
         The tech stack used in the job is: ${techstack}.
@@ -22,7 +22,7 @@ export async function POST(request: Request) {
         ["Question 1", "Question 2", "Question 3"]
         
         Thank you! <3
-    ,
+    `,
     });
 
     const interview = {
